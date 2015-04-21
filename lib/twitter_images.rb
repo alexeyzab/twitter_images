c38c5a66efaa6061c00c6ff80a9a6658cfa6d767
Alexey Zabelin <zabelin.alex@gmail.com>
2015-04-21 11:17:32 -0400

    Add Configure_credentials class

--- lib/twitter_images.rb
+++ lib/twitter_images.rb
@@ -92,20 +92,7 @@ module TwitterImages
     private
 
     def setup_credentials
-
-      puts "Please provide your twitter credentials now, first, enter your consumer key: "
-      consumer_key = gets.chomp
-      puts "Now your consumer secret: "
-      consumer_secret = gets.chomp
-
-      @consumer_key = OAuth::Consumer.new(consumer_key, consumer_secret)
-
-      puts "Your access token: "
-      access_token = gets.chomp
-      puts "Finally, your access token secret: "
-      access_secret = gets.chomp
-
-      @access_token = OAuth::Token.new(access_token, access_secret)
+      Configure_credentials.setup_credentials
     end
 
     def setup_http
