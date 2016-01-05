87223a1636fc4f5765ef7bd7ebf01b772e67c362
Alexey Zabelin <zabelin.alex@gmail.com>
2016-01-04 21:19:54 -0500

    Add CodeClimate payload fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 require "codeclimate-test-reporter"
+CodeClimate::TestReporter.configure do |config|
+  config.git_dir = `git rev-parse --show-toplevel`.strip
+end
 CodeClimate::TestReporter.start
 require "rspec"
 require "twitter_images"
