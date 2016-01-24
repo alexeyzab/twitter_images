0b1789cace00434ad2f023deb4deeda5b6ce50d6
Alexey Zabelin <zabelin.alex@gmail.com>
2016-01-24 18:19:33 -0500

    Add options hash params

--- lib/twitter_images/configuration.rb
+++ lib/twitter_images/configuration.rb
@@ -1,45 +1,43 @@
 module TwitterImages
   class Configuration
-    attr_reader   :requester
+    attr_reader   :options, :requester
     attr_accessor :search, :directory, :amount
 
-    def initialize(requester)
-      @requester = requester
+    def initialize
+      @requester = Requester.new
     end
 
-    def prepare
-      set_directory
-      get_search
-      get_amount
+    def prepare(options)
+      set_directory(options[:path])
+      get_search(options[:term])
+      get_amount(options[:amount])
       start
     end
 
     private
 
-    def set_directory
-      puts "Please enter the directory to save the images in: "
-      @directory = gets.chomp
+    def set_directory(dir)
+      @directory = File.expand_path(dir)
       raise StandardError, "Directory doesn't exist" unless directory_exists?
       change_directory
     end
 
     def directory_exists?
-      Dir.exist?(File.expand_path(@directory))
+      Dir.exist?(@directory)
     end
 
     def change_directory
-      Dir.chdir(File.expand_path(@directory))
+      Dir.chdir(@directory)
     end
 
-    def get_search
-      puts "Please enter the search terms: "
-      @search = gets.chomp.gsub(/\s/, "%20")
+    def get_search(term)
+      # @search = term.chomp.gsub(/\s/, "%20")
+      @search = term.chomp.gsub(/\s/, "%20").gsub(/\#/, "%23")
       raise StandardError, "The search string is empty" if @search.empty?
     end
 
-    def get_amount
-      puts "Please enter the number of images to download: "
-      @amount = gets.chomp.to_i
+    def get_amount(number)
+      @amount = number.to_i
       raise StandardError, "The number is too small" if @amount <= 0
     end
 
--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -4,113 +4,92 @@ describe TwitterImages::Configuration do
 
   describe "#initialize" do
     it "gets initialized with a requester" do
-      requester = double("Requester")
-      configuration = TwitterImages::Configuration.new(requester)
+      configuration = TwitterImages::Configuration.new
 
-      expect(configuration.requester).to eq(requester)
+      expect(configuration.requester).to be_a(TwitterImages::Requester)
     end
   end
 
   describe "#prepare" do
     it "calls the right methods to prepare the configuration" do
-      requester = double("Requester")
-      configuration = TwitterImages::Configuration.new(requester)
+      configuration = TwitterImages::Configuration.new
+      options = { path: "./test_pics", term: "cats", amount: "150" }
       allow(configuration).to receive(:set_directory)
       allow(configuration).to receive(:get_search)
       allow(configuration).to receive(:get_amount)
       allow(configuration).to receive(:start)
 
-      configuration.prepare
+      configuration.prepare(options)
 
-      expect(configuration).to have_received(:set_directory)
-      expect(configuration).to have_received(:get_search)
-      expect(configuration).to have_received(:get_amount)
+      expect(configuration).to have_received(:set_directory).with("./test_pics")
+      expect(configuration).to have_received(:get_search).with("cats")
+      expect(configuration).to have_received(:get_amount).with("150")
       expect(configuration).to have_received(:start)
     end
   end
 
   describe "#set_directory" do
     it "sets the directory" do
-      requester = double("Requester")
-      configuration = TwitterImages::Configuration.new(requester)
-      allow(configuration).to receive(:gets).and_return("#{Dir.getwd}\n")
+      configuration = TwitterImages::Configuration.new
 
-      configuration.send(:set_directory)
+      configuration.send(:set_directory, "#{Dir.getwd}")
 
       expect(configuration.directory).to eq("#{Dir.getwd}")
     end
 
     it "raises a StandardError when the entered directory does not exist" do
-      requester = double("Requester")
-      configuration = TwitterImages::Configuration.new(requester)
-      allow(configuration).to receive(:gets).and_return("/idontexist\n")
+      configuration = TwitterImages::Configuration.new
       allow(configuration).to receive(:directory_exists?).and_return(false)
 
-      expect { configuration.send(:set_directory) }.to raise_error(StandardError, "Directory doesn't exist")
+      expect { configuration.send(:set_directory, "#{Dir.getwd}") }.to raise_error(StandardError, "Directory doesn't exist")
     end
 
-    it "correctly parses the tilde sign" do
-      requester = double("Requester")
-      configuration = TwitterImages::Configuration.new(requester)
-      current_directory = Dir.getwd
-      allow(configuration).to receive(:gets).and_return("~\n")
+    it "correctly parses the relative path" do
+      configuration = TwitterImages::Configuration.new
 
-      expect { configuration.send(:set_directory) }.not_to raise_error
-
-      Dir.chdir(current_directory)
+      expect { configuration.send(:set_directory, "./") }.not_to raise_error
     end
   end
 
   describe "#get_search" do
     it "turns the search term into parameters" do
-      requester = double("Requester")
-      configuration = TwitterImages::Configuration.new(requester)
-      allow(configuration).to receive(:gets).and_return("Hi there\n")
+      configuration = TwitterImages::Configuration.new
+      configuration.send(:get_search, "#blessed weekend")
 
-      configuration.send(:get_search)
-
-      expect(configuration.search).to eq "Hi%20there"
+      expect(configuration.search).to eq "%23blessed%20weekend"
     end
 
     it "raises a StandardError if the search is empty" do
-      requester = double("Requester")
-      configuration = TwitterImages::Configuration.new(requester)
-      allow(configuration).to receive(:gets).and_return("\n")
+      configuration = TwitterImages::Configuration.new
 
-      expect { configuration.send(:get_search) }.to raise_error(StandardError, "The search string is empty")
+      expect { configuration.send(:get_search, "") }.to raise_error(StandardError, "The search string is empty")
     end
   end
 
   describe "#get_amount" do
     it "gets the amount of images to download as an integer" do
-      requester = double("Requester")
-      configuration = TwitterImages::Configuration.new(requester)
-      allow(configuration).to receive(:gets).and_return("3\n")
+      configuration = TwitterImages::Configuration.new
 
-      configuration.send(:get_amount)
+      configuration.send(:get_amount, "3")
 
       expect(configuration.amount).to eq(3)
     end
 
     it "raises a StandardError if the amount is 0 or less" do
-      requester = double("Requester")
-      configuration = TwitterImages::Configuration.new(requester)
-      allow(configuration).to receive(:gets).and_return("0\n")
+      configuration = TwitterImages::Configuration.new
 
-      expect { configuration.send(:get_amount) }.to raise_error(StandardError, "The number is too small")
+      expect { configuration.send(:get_amount, "0") }.to raise_error(StandardError, "The number is too small")
     end
   end
 
   describe "#start" do
     it "passes the right information to the Requester" do
-      requester = double("Requester", :start => true)
-      configuration = TwitterImages::Configuration.new(requester)
+      configuration = TwitterImages::Configuration.new
       configuration.search = "cats"
       configuration.amount = 200
+      expect(configuration.requester).to receive(:start).with("cats", 200)
 
       configuration.send(:start)
-
-      expect(requester).to have_received(:start).with("cats", 200)
     end
   end
 end
