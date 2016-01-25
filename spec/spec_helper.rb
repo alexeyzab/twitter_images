3467f3a7cd600882359f9e13aa9d5f20715f91e4
Alexey Zabelin <zabelin.alex@gmail.com>
2016-01-25 00:20:45 -0500

    Add a new WebMock stub

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,10 @@ RSpec.configure do |config|
       with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>//, 'User-Agent'=>'OAuth gem v0.4.7'}).
       to_return(:status => 200, :body => "", :headers => {})
 
+    stub_request(:get, "https://api.twitter.com/1.1/search/tweets.json?count=100&q=cats&result_type=recent").
+      with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>//, 'User-Agent'=>'OAuth gem v0.4.7'}).
+      to_return(:status => 200, :body => "", :headers => {})
+
     stub_request(:get, "http://pbs.twimg.com/media/123456789000000.jpg:large").
       with(:headers => {'User-Agent'=>'Typhoeus - https://github.com/typhoeus/typhoeus'}).
       to_return(:status => 200, :body => "", :headers => {})
